1975760070d3e066edece95fe2ecd68d1781c723
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-07-02 13:25:11 +0200

    Add files via upload

--- Bulk file rename.py
+++ Bulk file rename.py
@@ -1,56 +1,93 @@
 import os
 import shutil
+from pathlib import Path
 
 def backup_files(directory, backup_directory):
+    """
+    Create a backup of files in the specified directory.
+
+    Args:
+        directory (str): Source directory to backup files from.
+        backup_directory (str): Directory where the backup will be stored.
+
+    Returns:
+        None
+    """
     if not os.path.exists(backup_directory):
         os.makedirs(backup_directory)
-    for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            shutil.copy2(os.path.join(directory, filename), backup_directory)
-            print(f"Backed up: {filename}")
+
+    for entry in os.scandir(directory):
+        if entry.is_file():
+            shutil.copy2(entry.path, backup_directory)
+            print(f"Backed up: {entry.name}")
 
 def restore_files(directory, backup_directory):
-    for filename in os.listdir(backup_directory):
-        if os.path.isfile(os.path.join(backup_directory, filename)):
-            new_filename = os.path.splitext(filename)[0]
-            old_filepath = os.path.join(backup_directory, filename)
+    """
+    Restore files from the backup directory to the specified directory.
+
+    Args:
+        directory (str): Target directory to restore files to.
+        backup_directory (str): Directory containing the backup files.
+
+    Returns:
+        None
+    """
+    for entry in os.scandir(backup_directory):
+        if entry.is_file():
+            new_filename = os.path.splitext(entry.name)[0]
+            old_filepath = entry.path
             new_filepath = os.path.join(directory, new_filename)
             try:
                 os.rename(old_filepath, new_filepath)
-                print(f"Restored: {filename} --> {new_filename}")
+                print(f"Restored: {entry.name} --> {new_filename}")
             except Exception as e:
-                print(f"Error restoring {filename}: {str(e)}")
+                print(f"Error restoring {entry.name}: {str(e)}")
 
 def bulk_rename(directory, prefix, extension_filter=None, preview=False, interactive=False, backup=False):
-    counter = 1
-    renamed_files = []
-    backup_directory = None
+    """
+    Bulk rename files in the specified directory.
+
+    Args:
+        directory (str): Directory where the files to be renamed are located.
+        prefix (str): Prefix to be added to the new filenames.
+        extension_filter (str, optional): Filter for specific file extensions. Defaults to None.
+        preview (bool, optional): If True, preview the renaming without actually renaming the files. Defaults to False.
+        interactive (bool, optional): If True, prompt for user confirmation in case of conflicts. Defaults to False.
+        backup (bool, optional): If True, create a backup of the original files. Defaults to False.
+
+    Returns:
+        None
+    """
+    directory_path = Path(directory)
+    backup_directory = directory_path / "backup"
 
     if backup:
-        backup_directory = os.path.join(directory, "backup")
+        os.makedirs(backup_directory, exist_ok=True)
         backup_files(directory, backup_directory)
 
-    for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            file_extension = os.path.splitext(filename)[1]
-            if extension_filter is None or file_extension.lower() == extension_filter.lower():
+    renamed_files = []
+    counter = 1
+
+    for entry in os.scandir(directory_path):
+        if entry.is_file():
+            file_extension = entry.suffix
+            if not extension_filter or file_extension.lower() == extension_filter.lower():
                 new_filename = f"{prefix}_{counter:03}{file_extension}"
-                old_filepath = os.path.join(directory, filename)
-                new_filepath = os.path.join(directory, new_filename)
+                new_filepath = directory_path / new_filename
                 if preview:
-                    print(f"Preview - Renaming: {filename} --> {new_filename}")
+                    print(f"Preview - Renaming: {entry.name} --> {new_filename}")
                 else:
-                    if interactive and os.path.exists(new_filepath):
+                    if interactive and new_filepath.exists():
                         choice = input(f"Conflict: {new_filename} already exists. Replace it? (y/n): ")
                         if choice.lower() != 'y':
-                            print(f"Skipped: {filename}")
+                            print(f"Skipped: {entry.name}")
                             continue
                     try:
-                        os.rename(old_filepath, new_filepath)
-                        renamed_files.append((filename, new_filename))
-                        print(f"Renamed: {filename} --> {new_filename}")
+                        os.rename(entry.path, new_filepath)
+                        renamed_files.append((entry.name, new_filename))
+                        print(f"Renamed: {entry.name} --> {new_filename}")
                     except Exception as e:
-                        print(f"Error renaming {filename}: {str(e)}")
+                        print(f"Error renaming {entry.name}: {str(e)}")
                 counter += 1
 
     if not preview and len(renamed_files) > 0:
@@ -58,35 +95,59 @@ def bulk_rename(directory, prefix, extension_filter=None, preview=False, interac
         for old_name, new_name in renamed_files:
             print(f"{old_name} --> {new_name}")
 
-    if backup and backup_directory:
+    if backup:
         restore_option = input("Do you want to restore the original filenames? (y/n): ")
         if restore_option.lower() == "y":
             restore_files(directory, backup_directory)
         shutil.rmtree(backup_directory)
 
 def filter_files_by_size(directory, size_limit):
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
-            file_size = os.path.getsize(file_path)
-            if file_size <= size_limit:
-                print(f"Filtered: {filename} (Size: {file_size} bytes)")
+    """
+    Filter files in the specified directory based on their size.
+
+    Args:
+        directory (str): Directory to filter files from.
+        size_limit (int): Maximum size in bytes for the files to be included.
+
+    Returns:
+        List[str]: List of file paths that meet the size criteria.
+    """
+    filtered_files = []
+
+    for entry in os.scandir(directory):
+        if entry.is_file() and entry.stat().st_size <= size_limit:
+            filtered_files.append(entry.path)
+            print(f"Filtered: {entry.name} (Size: {entry.stat().st_size} bytes)")
+
+    return filtered_files
 
 def sort_files(directory):
-    file_list = os.listdir(directory)
-    file_list.sort()
-    for filename in file_list:
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
-            print(filename)
+    """
+    Sort and print files in the specified directory.
+
+    Args:
+        directory (str): Directory to sort files in.
+
+    Returns:
+        None
+    """
+    file_list = sorted(Path(directory).iterdir())
+    for entry in file_list:
+        if entry.is_file():
+            print(entry.name)
 
 def count_files(directory):
-    count = 0
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
-            count += 1
-    print(f"Total files: {count}")
+    """
+    Count and print the total number of files in the specified directory.
+
+    Args:
+        directory (str): Directory to count files in.
+
+    Returns:
+        None
+    """
+    file_count = sum(1 for entry in os.scandir(directory) if entry.is_file())
+    print(f"Total files: {file_count}")
 
 # Example usage
 directory_path = "/path/to/directory"  # Replace with the target directory path
@@ -99,6 +160,6 @@ backup_mode = True  # Set to True to create a backup of the original files, or F
 bulk_rename(directory_path, new_prefix, file_extension_filter, preview_mode, interactive_mode, backup_mode)
 
 # Additional function usage
-filter_files_by_size(directory_path, 1024)  # Filter files smaller than or equal to 1KB
+filtered_files = filter_files_by_size(directory_path, 1024)  # Filter files smaller than or equal to 1KB
 sort_files(directory_path)  # Sort files in the directory
 count_files(directory_path)  # Count the total number of files in the directory
--- CSV to Excel.py
+++ CSV to Excel.py
@@ -1,8 +1,25 @@
 import pandas as pd
 
+# Constants
+CSV_FILE_PATH = "path/to/input.csv"
+EXCEL_FILE_PATH = "path/to/output.xlsx"
+SORT_COLUMN_NAME = "column_name_to_sort"
+FILTER_COLUMN_NAME = "column_name_to_filter"
+FILTER_VALUE = "value_to_filter"
+GROUP_BY_COLUMNS = ["column1", "column2"]
+AGGREGATION_COLUMN = "column_to_aggregate"
+AGGREGATION_FUNCTION = "sum"
+CSV_FILES_TO_MERGE = ["path/to/file1.csv", "path/to/file2.csv", "path/to/file3.csv"]
+COLUMN_NAMES_TO_RENAME = {
+    "current_column_name1": "new_column_name1",
+    "current_column_name2": "new_column_name2",
+}
+COLUMNS_TO_APPLY_FUNCTION = ["column1", "column2"]
+
+# Utility functions
 def read_csv(csv_file):
     try:
-        data_frame = pd.read_csv(csv_file)
+        data_frame = pd.read_csv(csv_file, header=None, skip_blank_lines=True)
         return data_frame
     except FileNotFoundError:
         print(f"Error: File '{csv_file}' not found.")
@@ -18,7 +35,14 @@ def save_to_excel(data_frame, excel_file):
     except Exception as e:
         print(f"Error: Unable to save data to '{excel_file}': {e}")
 
-def sort_csv_to_excel(csv_file, excel_file, sort_column):
+# Data processing functions
+def remove_duplicates_and_save(csv_file, excel_file):
+    data_frame = read_csv(csv_file)
+    if data_frame is not None:
+        data_frame.drop_duplicates(inplace=True)
+        save_to_excel(data_frame, excel_file)
+
+def sort_and_save(csv_file, excel_file, sort_column):
     data_frame = read_csv(csv_file)
     if data_frame is not None:
         if sort_column not in data_frame.columns:
@@ -27,7 +51,7 @@ def sort_csv_to_excel(csv_file, excel_file, sort_column):
             sorted_data = data_frame.sort_values(by=sort_column)
             save_to_excel(sorted_data, excel_file)
 
-def filter_csv_to_excel(csv_file, excel_file, filter_column, filter_value):
+def filter_and_save(csv_file, excel_file, filter_column, filter_value):
     data_frame = read_csv(csv_file)
     if data_frame is not None:
         if filter_column not in data_frame.columns:
@@ -36,7 +60,7 @@ def filter_csv_to_excel(csv_file, excel_file, filter_column, filter_value):
             filtered_data = data_frame[data_frame[filter_column] == filter_value]
             save_to_excel(filtered_data, excel_file)
 
-def aggregate_csv_to_excel(csv_file, excel_file, group_by_columns, aggregation_column, aggregation_function):
+def aggregate_and_save(csv_file, excel_file, group_by_columns, aggregation_column, aggregation_function):
     data_frame = read_csv(csv_file)
     if data_frame is not None:
         columns_exist = all(column in data_frame.columns for column in group_by_columns + [aggregation_column])
@@ -46,71 +70,43 @@ def aggregate_csv_to_excel(csv_file, excel_file, group_by_columns, aggregation_c
             aggregated_data = data_frame.groupby(group_by_columns)[aggregation_column].agg(aggregation_function).reset_index()
             save_to_excel(aggregated_data, excel_file)
 
-def merge_csv_to_excel(csv_files, excel_file):
+def merge_and_save(csv_files, excel_file):
     data_frames = [read_csv(csv_file) for csv_file in csv_files]
     data_frames = [df for df in data_frames if df is not None]  # Remove any None values
     if data_frames:
         merged_data = pd.concat(data_frames)
         save_to_excel(merged_data, excel_file)
 
-def rename_columns(csv_file, excel_file, column_names):
+def rename_columns_and_save(csv_file, excel_file, column_names):
     data_frame = read_csv(csv_file)
     if data_frame is not None:
         data_frame.rename(columns=column_names, inplace=True)
         save_to_excel(data_frame, excel_file)
 
-def remove_duplicates(csv_file, excel_file):
-    data_frame = read_csv(csv_file)
-    if data_frame is not None:
-        data_frame.drop_duplicates(inplace=True)
-        save_to_excel(data_frame, excel_file)
-
-def apply_function_to_columns(csv_file, excel_file, columns, function):
+def apply_function_and_save(csv_file, excel_file, columns, function):
     data_frame = read_csv(csv_file)
     if data_frame is not None:
         data_frame[columns] = data_frame[columns].apply(function)
         save_to_excel(data_frame, excel_file)
 
 if __name__ == '__main__':
-    # Provide the paths to the CSV and Excel files
-    csv_file_path = "path/to/input.csv"
-    excel_file_path = "path/to/output.xlsx"
-
-    # Specify the column to sort by
-    sort_column_name = "column_name_to_sort"
-
-    # Specify the column and value to filter by
-    filter_column_name = "column_name_to_filter"
-    filter_value = "value_to_filter"
-
-    # Specify the columns to group by, column to aggregate, and aggregation function
-    group_by_columns = ["column1", "column2"]
-    aggregation_column = "column_to_aggregate"
-    aggregation_function = "sum"
-
-    # Specify the list of CSV files to merge
-    csv_files_to_merge = ["path/to/file1.csv", "path/to/file2.csv", "path/to/file3.csv"]
-
     # Call the function to rename the columns and save the modified data to Excel
-    column_names_to_rename = {"current_column_name1": "new_column_name1", "current_column_name2": "new_column_name2"}
-    rename_columns(csv_file_path, excel_file_path, column_names_to_rename)
+    rename_columns_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH, COLUMN_NAMES_TO_RENAME)
 
     # Call the function to remove duplicates and save the modified data to Excel
-    remove_duplicates(csv_file_path, excel_file_path)
+    remove_duplicates_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH)
 
     # Call the function to apply a function to columns and save the modified data to Excel
-    columns_to_apply_function = ["column1", "column2"]
-    function_to_apply = lambda x: x.upper()  # Example function, you can replace it with your desired function
-    apply_function_to_columns(csv_file_path, excel_file_path, columns_to_apply_function, function_to_apply)
+    apply_function_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH, COLUMNS_TO_APPLY_FUNCTION, lambda x: x.upper())
 
     # Call the function to sort the data and save it to Excel
-    sort_csv_to_excel(csv_file_path, excel_file_path, sort_column_name)
+    sort_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH, SORT_COLUMN_NAME)
 
     # Call the function to filter the data and save it to Excel
-    filter_csv_to_excel(csv_file_path, excel_file_path, filter_column_name, filter_value)
+    filter_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH, FILTER_COLUMN_NAME, FILTER_VALUE)
 
     # Call the function to aggregate the data and save it to Excel
-    aggregate_csv_to_excel(csv_file_path, excel_file_path, group_by_columns, aggregation_column, aggregation_function)
+    aggregate_and_save(CSV_FILE_PATH, EXCEL_FILE_PATH, GROUP_BY_COLUMNS, AGGREGATION_COLUMN, AGGREGATION_FUNCTION)
 
     # Call the function to merge the CSV files and save the merged data to Excel
-    merge_csv_to_excel(csv_files_to_merge, excel_file_path)
+    merge_and_save(CSV_FILES_TO_MERGE, EXCEL_FILE_PATH)
